1e070c6708f76e42390e8ca4ce50d68540bd3978
ayoubHam2000 <ayoubbenhamou0731@gmail.com>
2022-03-01 21:01:14 +0100

    login

--- app/src/main/java/com/example/watchmovie/MainActivity.kt
+++ app/src/main/java/com/example/watchmovie/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.watchmovie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import com.example.watchmovie.Services.JsonTask
@@ -11,26 +12,60 @@ import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var button : Button
-    lateinit var output : TextView
-    lateinit var image : ImageView
+    lateinit var userNameField : EditText
+    lateinit var passwordField : EditText
+    lateinit var signInBtn : Button
+    lateinit var loginBtn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        output = findViewById(R.id.output)
-        button = findViewById(R.id.button)
-        image = findViewById(R.id.image)
-
-        button.setOnClickListener {
-            val url = "http://192.168.0.65:8000/"
-            //getJson(url)
-            postJson((url))
-        }
+        initView()
 
     }
 
+    private fun initView()
+    {
+        userNameField = this.findViewById(R.id.userName)
+        passwordField = this.findViewById(R.id.password)
+        signInBtn = this.findViewById(R.id.logIn)
+        loginBtn = this.findViewById(R.id.signIn)
+
+        loginBtn.setOnClickListener{logInClick()}
+    }
+
+    private fun logInClick()
+    {
+        //trim is for to remove spaces from start and end ex: "  aasaad " => "aasaad"
+        val userName = userNameField.text.trim()
+        val password = userNameField.text.trim()
+        when {
+            userName.isEmpty() -> userNameField.error = "Empty Field"
+            password.isEmpty() -> passwordField.error = "Empty Field"
+            else -> {
+                val obj = JSONObject();
+                obj.put("user_name", userName)
+                obj.put("password", password)
+                postJson("http://10.11.2.10:8080/user", obj)
+            }
+        }
+    }
+
+    private fun postJson(url : String, obj : JSONObject)
+    {
+        JsonTask.postJsonObject(this, url, obj){success, res->
+            if (success){
+                println(">>$res")
+            }
+            else
+            {
+                println("Error")
+            }
+        }
+    }
+
+/*
     private fun getJson(url : String)
     {
 
@@ -57,5 +92,5 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
+*/
 }
